2663708444aa16113d320b0bf31d339bb36eb709
ids6 <ids6@live.com>
2024-12-06 05:04:55 +0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+import colors from 'tailwindcss/colors';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        // используется для ссылок и выделения текущего элемента;
+        accent: colors.indigo,
+        // используется для цветов фона и границ;
+        gray: colors.zinc,
+      },
+      fontFamily: {
+        // используется для текста в интерфейсе и контента;
+        sans: ['"Atkinson Hyperlegible"'],
+        // используется для примеров кода.
+        mono: ['"IBM Plex Mono"'],
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
+};
